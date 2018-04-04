031a1dbae225b67465351290ef8cb9facb1d7fce
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-04 02:53:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM subgenres
+  FROM authors
   JOIN authors ON subgenres.id = authors.subgenres_id "
 end
 
