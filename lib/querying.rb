f211534b6bb33a4968014ad4d555c239c9b9fd37
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-04 03:20:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.value "
+  "SELECT characters.species
+  FROM characters
+  ORDER BY COUNT(characters.species) DESC limit 1
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
